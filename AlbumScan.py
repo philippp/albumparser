0e19789a27a7947d7e20a5a92ba5903fd5a11c39
Philipp Pfeiffenberger <philippp@philipp-pfeiffenbergers-mac-mini.local>
2009-02-26 01:43:21 -0800

    Added initial album/artist recognition. If two directories (parent and child) are the same, we split based on "-" and assume the album does not have a hyphen. Splitting on "-" is bound to fail (ex: Phil-UFO - My Yo-Yo) at some point, but we're going for max coverage.

--- AlbumScan.py
+++ AlbumScan.py
@@ -1,17 +1,49 @@
 import os
 import stat
 import config
+import re
 
-def treeEntryToAlbum(path, trackGrouping):
+def decodeDirName(path, trackGrouping):
     '''Given a directory tree entry, semantically deduce whether
     it is an album. 
     Two cases are considered:
     Artist - Album 
     Artist / Album
     ''' 
+    dirNamesClean, dirNamesSort = [], []
     dirDepth = len(path.split(os.sep)) - len(config.rootDir.split(os.sep))
     dirs = path.split(os.sep)[0-dirDepth:]
-    print "%r deep, %r entries\n%s\n-------" % (dirDepth, len(trackGrouping), path)
+    
+    for idx, dirName in enumerate(dirs):
+       dirNameClean, dirNameSort = nameToParts( dirName )
+       dirNamesClean.append( dirNameClean )
+       dirNamesSort.append( dirNameSort )
+    if len(dirNamesSort) == 1 or cmp(dirNamesSort[0], dirNamesSort[1]) == 0:
+        artist, album = cleanNameParse( dirNamesClean[0] )
+    else:
+        artist = dirNamesClean[0]
+        album = dirNamesClean[1]
+
+    return artist, album
+
+
+def cleanNameParse( cleanName ):
+    nameParts = cleanName.split("-")
+    artistName = "-".join(nameParts[0:-1])
+    albumName = nameParts[-1:]
+    return artistName, albumName
+
+def nameToParts( dirName ):
+    ''' Filters unused information from a directory name and splits each name by -
+    returns (cleanNameParts, sortNameParts)'''
+
+    encLabel = '(\([vV\dflac]*\))*'    #(v0) (flac) etc
+    miscLabel = '(\[.*\])*'            #square brackets [2008]
+    cleanName = re.sub(encLabel+miscLabel,'',dirName)
+    sortName = "".join(re.findall('([\d\w\-]+)',cleanName)).lower()    
+    return cleanName, sortName
+
+
 
 def scanTree(path):
     '''Recursively scan a directory tree and find groups of albums'''
@@ -27,7 +59,10 @@ def scanTree(path):
             trackGroupings[parentEntryPath].append((entryPath, entry))
     return trackGroupings
 
+
 trackGroupings = scanTree( config.rootDir )
 
-for key, val in trackGroupings.items():
-    treeEntryToAlbum(key, val)
+for dirName, trackGrouping in trackGroupings.items():
+    artist, album = decodeDirName(dirName, trackGrouping)
+    print "Artist: %s\nAlbum: %s" % (artist, album)
+    print "%r entries\n%s\n-------" % (len(trackGrouping), dirName)     
